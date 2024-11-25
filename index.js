b18cae4680e6342eda97bb1493906fd1410bb773
AngelGD <angelgabriel.gd@gmail.com>
2024-11-25 10:34:49 -0500

    add library prettier --dev

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import _ from "lodash"
+import _ from "lodash";
+const O = "hola";
 
-console.log(_.intersection([2,1],[2,3]));
+console.log(_.intersection([2, 1], [2, 3]));
+
+if ((O = "hola")) {
+  console.log(O);
+  let g = 45;
+}
 
 //console.log('Hello Angel, desde wsl-ubuntu-git');
